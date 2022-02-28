8282f9b248802445b94b4279c00418b9b219db2c
Konstantin Voykov <koykov0@gmail.com>
2022-02-28 22:49:57 +0200

    Intermediate commit.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <b-container fluid
+               tag="main"
+               id="app"
+               v-cloak>
+    <b-navbar toggleable="md"
+              class="mb-3"
+              type="light"
+              variant="light">
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-collapse is-nav
+                  id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#"
+                      disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm"
+                          class="mr-sm-2"
+                          type="text"
+                          placeholder="Search" />
+            <b-button size="sm"
+                      class="my-2 my-sm-0"
+                      type="submit">Search</b-button>
+          </b-nav-form>
+          <b-nav-item-dropdown text="Lang"
+                               right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <header class="text-center">
+      <a href="https://github.com/alexsasharegan/vue-transmit"
+         target="_blank">
+        <img class="img--logo"
+             src="https://raw.githubusercontent.com/alexsasharegan/vue-transmit/master/docs/logo.png" alt="">
+      </a>
+      <h1 class="mb-5"><code>&lt;vue-transmit&gt;</code></h1>
+    </header>
+    <b-container tag="main">
+      <b-row>
+        <b-col cols="3"></b-col>
+        <b-col cols="6">
+
+        </b-col>
+      </b-row>
+      <b-row class="my-3">
+
+      </b-row>
+    </b-container>
+    <b-modal v-model="showModal"
+             title="File Upload: Error">
+      <p class="bg-danger text-white p-3 my-2"
+         v-html="error"></p>
+    </b-modal>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      // ...
+    };
+  },
+  methods: {
+    onError(file, errorMsg) {
+      this.error = errorMsg;
+      this.showModal = true;
+    }
+  },
+  filters: {
+    json(value) {
+      return JSON.stringify(value, null, 2);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-
 import BootstrapVue from 'bootstrap-vue'
+import VueFlex from "vue-flex";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.use(VueFlex);
