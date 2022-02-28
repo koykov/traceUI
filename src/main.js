5fa3351b03ae2d058edbc0d159a259f6ab6c653b
Konstantin Voykov <koykov0@gmail.com>
2022-02-28 22:58:51 +0200

    Remove unneeded plugins.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueFlex from "vue-flex";
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue);
+Vue.use(VueFlex);
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+});
--- src/plugins/bootstrap-vue.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import VueFlex from "vue-flex";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue);
-Vue.use(VueFlex);
