b819c25274745971a66faa239b3234356cf9ca71
Konstantin Voykov <koykov0@gmail.com>
2022-03-23 23:54:53 +0200

    Fetch router params.

--- src/views/ViewTrace.vue
+++ src/views/ViewTrace.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    foobar
+    {{ tid }}
   </div>
 </template>
 
@@ -9,7 +9,7 @@ export default {
   name: "ViewTrace",
   data() {
     return {
-      //
+      tid: this.$route.params.id
     };
   }
 }
