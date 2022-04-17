37a44fc8cd3ab9afac42cf1ed5c1a8cdc2fac7cc
Konstantin Voykov <koykov0@gmail.com>
2022-04-18 00:16:09 +0300

    Tree rendering draft.

--- src/views/ViewTrace.vue
+++ src/views/ViewTrace.vue
@@ -20,115 +20,119 @@
 
       <div class="t-log">
         <div class="t-th-tree">
-          <svg width="48" height="1040" focusable="false" aria-hidden="true" class="t-flow-container">
+          <svg :width="treeWH.w" :height="treeWH.h" focusable="false" aria-hidden="true" class="t-flow-container">
             <g transform="translate(8, 54)">
               <rect x="-8" y="8" width="16" height="1000" class="t-flow-rect"></rect>
-              <path d="M0,0 L0,40" class="css-1iinw0h"></path>
-              <circle cx="0" cy="0" r="3" class="css-1e88pln"></circle>
-              <path d="M0,40 L0,80" class="css-1iinw0h"></path>
-              <path d="M0,40 C0,60 8,60 8,80" class="css-dok1lu"></path>
-              <circle cx="0" cy="40" r="3" class="css-1e88pln"></circle>
-              <path d="M0,80 L0,120" class="css-1iinw0h"></path>
-              <path d="M8,80 L8,120" class="css-dok1lu"></path>
-              <circle cx="8" cy="80" r="3" class="css-84yyhk"></circle>
-              <path d="M0,120 L0,160" class="css-1iinw0h"></path>
-              <path d="M8,120 L8,160" class="css-dok1lu"></path>
-              <path d="M8,120 C8,140 0,140 0,160" class="css-1iinw0h"></path>
-              <circle cx="8" cy="120" r="3" class="css-84yyhk"></circle>
-              <path d="M0,160 L0,200" class="css-1iinw0h"></path>
-              <path d="M0,160 C0,180 8,180 8,200" class="css-dok1lu"></path>
-              <path d="M8,160 L8,200" class="css-dok1lu"></path>
-              <circle cx="0" cy="160" r="3" class="css-1e88pln"></circle>
-              <path d="M0,200 L0,240" class="css-1iinw0h"></path>
-              <path d="M8,200 L8,240" class="css-dok1lu"></path>
-              <circle cx="8" cy="200" r="3" class="css-84yyhk"></circle>
-              <path d="M0,240 L0,280" class="css-1iinw0h"></path>
-              <path d="M8,240 L8,280" class="css-dok1lu"></path>
-              <path d="M8,240 C8,260 0,260 0,280" class="css-1iinw0h"></path>
-              <circle cx="8" cy="240" r="3" class="css-84yyhk"></circle>
-              <path d="M0,280 L0,320" class="css-1iinw0h"></path>
-              <path d="M8,280 L8,320" class="css-dok1lu"></path>
-              <path d="M16,280 L16,320" class="css-1rs47pa"></path>
-              <circle cx="16" cy="280" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,320 L0,360" class="css-1iinw0h"></path>
-              <path d="M0,320 C0,340 16,340 16,360" class="css-1rs47pa"></path>
-              <path d="M8,320 L8,360" class="css-dok1lu"></path>
-              <path d="M16,320 L16,360" class="css-1rs47pa"></path>
-              <circle cx="0" cy="320" r="3" class="css-1e88pln"></circle>
-              <path d="M0,360 L0,400" class="css-1iinw0h"></path>
-              <path d="M8,360 L8,400" class="css-dok1lu"></path>
-              <path d="M16,360 L16,400" class="css-1rs47pa"></path>
-              <circle cx="16" cy="360" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,400 L0,440" class="css-1iinw0h"></path>
-              <path d="M8,400 L8,440" class="css-dok1lu"></path>
-              <path d="M16,400 L16,440" class="css-1rs47pa"></path>
-              <circle cx="16" cy="400" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,440 L0,480" class="css-1iinw0h"></path>
-              <path d="M8,440 L8,480" class="css-dok1lu"></path>
-              <path d="M16,440 L16,480" class="css-1rs47pa"></path>
-              <circle cx="16" cy="440" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,480 L0,520" class="css-1iinw0h"></path>
-              <path d="M8,480 L8,520" class="css-dok1lu"></path>
-              <path d="M16,480 L16,520" class="css-1rs47pa"></path>
-              <circle cx="16" cy="480" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,520 L0,560" class="css-1iinw0h"></path>
-              <path d="M8,520 L8,560" class="css-dok1lu"></path>
-              <path d="M16,520 L16,560" class="css-1rs47pa"></path>
-              <path d="M16,520 C16,540 0,540 0,560" class="css-1iinw0h"></path>
-              <circle cx="16" cy="520" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,560 L0,600" class="css-1iinw0h"></path>
-              <path d="M0,560 C0,580 8,580 8,600" class="css-jdft62"></path>
-              <path d="M8,560 C8,580 16,580 16,600" class="css-dok1lu"></path>
-              <path d="M16,560 C16,580 24,580 24,600" class="css-1rs47pa"></path>
-              <circle cx="0" cy="560" r="3" class="css-1e88pln"></circle>
-              <path d="M0,600 L0,640" class="css-1iinw0h"></path>
-              <path d="M8,600 L8,640" class="css-jdft62"></path>
-              <path d="M16,600 L16,640" class="css-dok1lu"></path>
-              <path d="M24,600 L24,640" class="css-1rs47pa"></path>
-              <circle cx="8" cy="600" r="3" class="css-1t617le"></circle>
-              <path d="M0,640 L0,680" class="css-1iinw0h"></path>
-              <path d="M0,640 C0,660 8,660 8,680" class="css-jdft62"></path>
-              <path d="M8,640 L8,680" class="css-jdft62"></path>
-              <path d="M16,640 L16,680" class="css-dok1lu"></path>
-              <path d="M24,640 L24,680" class="css-1rs47pa"></path>
-              <circle cx="0" cy="640" r="3" class="css-1e88pln"></circle>
-              <path d="M0,680 L0,720" class="css-1iinw0h"></path>
-              <path d="M8,680 C8,700 0,700 0,720" class="css-1iinw0h"></path>
-              <path d="M16,680 C16,700 8,700 8,720" class="css-dok1lu"></path>
-              <path d="M24,680 C24,700 16,700 16,720" class="css-1rs47pa"></path>
-              <circle cx="8" cy="680" r="3" class="css-1t617le"></circle>
-              <path d="M0,720 L0,760" class="css-1iinw0h"></path>
-              <path d="M0,720 C0,740 16,740 16,760" class="css-1rs47pa"></path>
-              <path d="M8,720 L8,760" class="css-dok1lu"></path>
-              <path d="M16,720 L16,760" class="css-1rs47pa"></path>
-              <circle cx="0" cy="720" r="3" class="css-1e88pln"></circle>
-              <path d="M0,760 L0,800" class="css-1iinw0h"></path>
-              <path d="M8,760 L8,800" class="css-dok1lu"></path>
-              <path d="M16,760 L16,800" class="css-1rs47pa"></path>
-              <circle cx="16" cy="760" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,800 L0,840" class="css-1iinw0h"></path>
-              <path d="M0,800 C0,820 8,820 8,840" class="css-10310f2"></path>
-              <path d="M8,800 C8,820 16,820 16,840" class="css-dok1lu"></path>
-              <path d="M16,800 C16,820 24,820 24,840" class="css-1rs47pa"></path>
-              <circle cx="0" cy="800" r="3" class="css-1e88pln"></circle>
-              <path d="M0,840 L0,880" class="css-1iinw0h"></path>
-              <path d="M8,840 C8,860 0,860 0,880" class="css-1iinw0h"></path>
-              <path d="M16,840 C16,860 8,860 8,880" class="css-dok1lu"></path>
-              <path d="M24,840 C24,860 16,860 16,880" class="css-1rs47pa"></path>
-              <circle cx="8" cy="840" r="3" class="css-bakhhg"></circle>
-              <path d="M0,880 L0,920" class="css-1iinw0h"></path>
-              <path d="M0,880 C0,900 16,900 16,920" class="css-1rs47pa"></path>
-              <path d="M8,880 L8,920" class="css-dok1lu"></path>
-              <path d="M16,880 L16,920" class="css-1rs47pa"></path>
-              <circle cx="0" cy="880" r="3" class="css-1e88pln"></circle>
-              <path d="M0,920 L0,960" class="css-1iinw0h"></path>
-              <path d="M8,920 L8,960" class="css-dok1lu"></path>
-              <path d="M16,920 L16,960" class="css-1rs47pa"></path>
-              <circle cx="16" cy="920" r="3" class="css-1ylas8d"></circle>
-              <path d="M0,960 L0,1000" class="css-1iinw0h"></path>
-              <path d="M8,960 L8,1000" class="css-dok1lu"></path>
-              <path d="M16,960 L16,1000" class="css-1rs47pa"></path>
-              <circle cx="16" cy="960" r="3" class="css-1ylas8d"></circle>
+              <template v-for="(itm, i) in tree">
+                <circle :key="i" v-if="itm.typ === 'circle'" :cx="itm.x" :cy="itm.y" r="3" class="css-1e88pln"></circle>
+                <path :key="i" v-if="itm.typ === 'line'" :d="`M${itm.x0},${itm.y0} L${itm.xN},${itm.yN}`" class="css-1iinw0h"></path>
+              </template>
+<!--              <path d="M0,0 L0,40" class="css-1iinw0h"></path>-->
+<!--              <circle cx="0" cy="0" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,40 L0,80" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,40 C0,60 8,60 8,80" class="css-dok1lu"></path>-->
+<!--              <circle cx="0" cy="40" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,80 L0,120" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,80 L8,120" class="css-dok1lu"></path>-->
+<!--              <circle cx="8" cy="80" r="3" class="css-84yyhk"></circle>-->
+<!--              <path d="M0,120 L0,160" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,120 L8,160" class="css-dok1lu"></path>-->
+<!--              <path d="M8,120 C8,140 0,140 0,160" class="css-1iinw0h"></path>-->
+<!--              <circle cx="8" cy="120" r="3" class="css-84yyhk"></circle>-->
+<!--              <path d="M0,160 L0,200" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,160 C0,180 8,180 8,200" class="css-dok1lu"></path>-->
+<!--              <path d="M8,160 L8,200" class="css-dok1lu"></path>-->
+<!--              <circle cx="0" cy="160" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,200 L0,240" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,200 L8,240" class="css-dok1lu"></path>-->
+<!--              <circle cx="8" cy="200" r="3" class="css-84yyhk"></circle>-->
+<!--              <path d="M0,240 L0,280" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,240 L8,280" class="css-dok1lu"></path>-->
+<!--              <path d="M8,240 C8,260 0,260 0,280" class="css-1iinw0h"></path>-->
+<!--              <circle cx="8" cy="240" r="3" class="css-84yyhk"></circle>-->
+<!--              <path d="M0,280 L0,320" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,280 L8,320" class="css-dok1lu"></path>-->
+<!--              <path d="M16,280 L16,320" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="280" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,320 L0,360" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,320 C0,340 16,340 16,360" class="css-1rs47pa"></path>-->
+<!--              <path d="M8,320 L8,360" class="css-dok1lu"></path>-->
+<!--              <path d="M16,320 L16,360" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="320" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,360 L0,400" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,360 L8,400" class="css-dok1lu"></path>-->
+<!--              <path d="M16,360 L16,400" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="360" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,400 L0,440" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,400 L8,440" class="css-dok1lu"></path>-->
+<!--              <path d="M16,400 L16,440" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="400" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,440 L0,480" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,440 L8,480" class="css-dok1lu"></path>-->
+<!--              <path d="M16,440 L16,480" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="440" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,480 L0,520" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,480 L8,520" class="css-dok1lu"></path>-->
+<!--              <path d="M16,480 L16,520" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="480" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,520 L0,560" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,520 L8,560" class="css-dok1lu"></path>-->
+<!--              <path d="M16,520 L16,560" class="css-1rs47pa"></path>-->
+<!--              <path d="M16,520 C16,540 0,540 0,560" class="css-1iinw0h"></path>-->
+<!--              <circle cx="16" cy="520" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,560 L0,600" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,560 C0,580 8,580 8,600" class="css-jdft62"></path>-->
+<!--              <path d="M8,560 C8,580 16,580 16,600" class="css-dok1lu"></path>-->
+<!--              <path d="M16,560 C16,580 24,580 24,600" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="560" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,600 L0,640" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,600 L8,640" class="css-jdft62"></path>-->
+<!--              <path d="M16,600 L16,640" class="css-dok1lu"></path>-->
+<!--              <path d="M24,600 L24,640" class="css-1rs47pa"></path>-->
+<!--              <circle cx="8" cy="600" r="3" class="css-1t617le"></circle>-->
+<!--              <path d="M0,640 L0,680" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,640 C0,660 8,660 8,680" class="css-jdft62"></path>-->
+<!--              <path d="M8,640 L8,680" class="css-jdft62"></path>-->
+<!--              <path d="M16,640 L16,680" class="css-dok1lu"></path>-->
+<!--              <path d="M24,640 L24,680" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="640" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,680 L0,720" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,680 C8,700 0,700 0,720" class="css-1iinw0h"></path>-->
+<!--              <path d="M16,680 C16,700 8,700 8,720" class="css-dok1lu"></path>-->
+<!--              <path d="M24,680 C24,700 16,700 16,720" class="css-1rs47pa"></path>-->
+<!--              <circle cx="8" cy="680" r="3" class="css-1t617le"></circle>-->
+<!--              <path d="M0,720 L0,760" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,720 C0,740 16,740 16,760" class="css-1rs47pa"></path>-->
+<!--              <path d="M8,720 L8,760" class="css-dok1lu"></path>-->
+<!--              <path d="M16,720 L16,760" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="720" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,760 L0,800" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,760 L8,800" class="css-dok1lu"></path>-->
+<!--              <path d="M16,760 L16,800" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="760" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,800 L0,840" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,800 C0,820 8,820 8,840" class="css-10310f2"></path>-->
+<!--              <path d="M8,800 C8,820 16,820 16,840" class="css-dok1lu"></path>-->
+<!--              <path d="M16,800 C16,820 24,820 24,840" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="800" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,840 L0,880" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,840 C8,860 0,860 0,880" class="css-1iinw0h"></path>-->
+<!--              <path d="M16,840 C16,860 8,860 8,880" class="css-dok1lu"></path>-->
+<!--              <path d="M24,840 C24,860 16,860 16,880" class="css-1rs47pa"></path>-->
+<!--              <circle cx="8" cy="840" r="3" class="css-bakhhg"></circle>-->
+<!--              <path d="M0,880 L0,920" class="css-1iinw0h"></path>-->
+<!--              <path d="M0,880 C0,900 16,900 16,920" class="css-1rs47pa"></path>-->
+<!--              <path d="M8,880 L8,920" class="css-dok1lu"></path>-->
+<!--              <path d="M16,880 L16,920" class="css-1rs47pa"></path>-->
+<!--              <circle cx="0" cy="880" r="3" class="css-1e88pln"></circle>-->
+<!--              <path d="M0,920 L0,960" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,920 L8,960" class="css-dok1lu"></path>-->
+<!--              <path d="M16,920 L16,960" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="920" r="3" class="css-1ylas8d"></circle>-->
+<!--              <path d="M0,960 L0,1000" class="css-1iinw0h"></path>-->
+<!--              <path d="M8,960 L8,1000" class="css-dok1lu"></path>-->
+<!--              <path d="M16,960 L16,1000" class="css-1rs47pa"></path>-->
+<!--              <circle cx="16" cy="960" r="3" class="css-1ylas8d"></circle>-->
             </g>
           </svg>
         </div>
@@ -209,8 +213,8 @@ export default {
       trace: this.trace,
       service: this.service,
 
-      flowH: 40,
-      flowW: 8,
+      tree: this.tree,
+      treeWH: this.treeWH,
     };
   },
   mounted() {
@@ -226,13 +230,21 @@ export default {
           }
         })
         .then(response => {
+          const loff = 8;
+          const boff = 34.59;
+
           this.traceOK = true;
           this.trace = response.payload;
+          this.treeWH = {w: 0, h: 0}
+          this.tree = [];
+          this.tmsk = [true];
 
           let sid = this.$route.params.sid;
           for (let i in this.trace.services) {
             let svc = this.trace.services[i];
             if (svc.ID === sid || (sid === undefined && i === '0')) {
+              this.treeWH.w = (svc.threads + 2) * loff;
+              let bo = 0;
               for (i in svc.records) {
                 let rec = svc.records[i];
                 if (rec.rows !== undefined) {
@@ -252,8 +264,37 @@ export default {
                     text: rec.thread.type,
                     dt: rec.thread.dt,
                   });
+                  switch (rec.thread.type) {
+                    case 'TH_ACQ':
+                      this.tmsk[rec.childID] = true;
+                      break;
+                    case 'TH_REL':
+                      this.tmsk[rec.childID] = false;
+                      break;
+                  }
                 }
+                for (i in this.tmsk) {
+                  if (!this.tmsk[i]) {
+                    continue;
+                  }
+                  this.tree.push({
+                    typ: "circle",
+                    x: i * loff,
+                    y: bo,
+                  });
+                  this.tree.push({
+                    typ: "line",
+                    x0: i * loff,
+                    y0: bo,
+                    xN: i * loff,
+                    yN: bo + boff,
+                  });
+                }
+                bo += boff;
+                console.log(this.tmsk);
               }
+              this.treeWH.h = bo + boff;
+              console.log(this.tree);
             }
           }
         })
