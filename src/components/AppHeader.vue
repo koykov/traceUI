df312a498045fc43a1680698c04bfdd3a45a2319
Konstantin Voykov <koykov0@gmail.com>
2022-04-20 23:28:40 +0300

    Simplify header and footer components.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,10 +7,10 @@
     <b-navbar-brand href="/">traceID</b-navbar-brand>
     <b-collapse is-nav
                 id="nav_collapse">
-      <b-navbar-nav>
-        <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
+<!--      <b-navbar-nav>-->
+<!--        <b-nav-item :to="{name: 'home'}">Home</b-nav-item>-->
+<!--        <b-nav-item href="#" disabled>Disabled</b-nav-item>-->
+<!--      </b-navbar-nav>-->
 
       <b-navbar-nav class="ml-auto">
         <b-nav-form @submit.stop.prevent="submitGOTO">
